d90c110a5a2f4dc5cae1a529daf3d6d376bb675c
chimbi <chimbi@chimbi.com>
2026-03-06 19:21:49 +0100

    changed color to red

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <h1>{{ message }}</h1>
-    <p>Laravel + Inertia + Vue is working! 🎉 🤣🤣🤣🤣</p>
-  </div>
-  <div style="color:blue;font-size:24px;"> test text</div>
+    <div>
+        <h1>{{ message }}</h1>
+        <p>Laravel + Inertia + Vue is working! 🎉 🤣🤣🤣🤣</p>
+    </div>
+    <div style="color: red; font-size: 24px">test text</div>
 </template>
 
 <script setup>
 defineProps({
-  message: String
-})
+    message: String,
+});
 </script>
